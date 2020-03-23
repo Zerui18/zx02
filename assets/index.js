16a06cea804e97a34b6ffa084ef5f458c77d1d86
Zerui Chen <chenzerui2002@gmail.com>
2020-03-23 10:27:42 +0800

    halfway test

--- assets/index.js
+++ assets/index.js
@@ -1,25 +1,142 @@
 // Const Elements
-const tweaksDiv = document.getElementById("tweaks");
-const tweakOverlay = document.getElementById("tweak_overlay");
-const tweakOverlayCard = document.getElementById("tweak_overlay_card");
-const tweaks = [
-  {
-    name : "Frame",
-    description : "Bring your iDevice to life with your favourite video wallpapers!",
-    version : "2.0.0",
-    iconName : "ic_tweak_frame.png"
-  }
-]
+const mainElements = {
+  main : document.getElementById("main"),
+  tweaksDiv : document.getElementById("tweaks"),
+  tweakOverlayCard : document.getElementById("tweak_overlay_card")
+};
 
-// Funcs
-function setOverlayDisplaying(tweak) {
-  
+const modalElements = {
+  tweakIcon : document.getElementById("tweak_icon"),
+  tweakName : document.getElementById("tweak_name"),
+  tweakSubtitle : document.getElementById("tweak_subtitle"),
+  tweakGet : document.getElementById("tweak_get"),
+  tweakMiscs : document.getElementById("tweak_miscs"),
+  tweakScreens : document.getElementById("tweak_screens"),
+  tweakDescriptions : document.getElementById("tweak_descriptions")
 }
 
+let iOSVersion;
+
+if (navigator.appVersion.indexOf("iPhone OS") != -1)
+  iOSVersion = navigator.appVersion.match(/iPhone OS (\d+_\d_\d)/)[1].split("_").map(x => parseInt(x));
+
+// -1 if v1 is smaller, 0 if equal, 1 if v1 is larger.
+function compareVersions(v1, v2) {
+  for(let i = 0; i < 3; i++)
+    if (v1[i] < v2[i])
+      return -1;
+    else if (i == 2 && v1[i] == v2[i])
+    // Reached the end but still equal.
+      return 0;
+  return 1;
+}
+
+// Shorthand function for comparing a version to the current device's version,
+// returns -1 if the current device's iOS version cannot be read.
+function comparedToCurrentVersion(v) {
+  if (!iOSVersion)
+    return -1;
+  return compareVersions(v, iOSVersion);
+}
+
+// Funcs
+
+var removeChildren = function (node) {
+  let last;
+  while (last = node.lastChild)
+    node.removeChild(last);
+};
+
+// Update the contents of the overlay with the given tweak info.
+function setOverlayDisplaying(tweak) {
+  const details = tweak.details;
+  modalElements.tweakIcon.src = `./assets/icons/${tweak.id}.png`;
+  modalElements.tweakName.textContent = tweak.name;
+  modalElements.tweakSubtitle.textContent = tweak.details.subtitle;
+
+  // Determine which package should be served, or none at all.
+  // Can't believe I'm actually doing this lol.
+  for(let i in details.packages) {
+    // Check if minOSVersion is smaller than current version.
+    // It is required that the packages are listed in descending order of minOSVersion
+    // for this to pick the best package for each version.
+    const minOSVersion = details.packages[i].minOSVersion;
+    if (comparedToCurrentVersion(minOSVersion) == -1) {
+      const id = details.packages[i].id;
+      modalElements.tweakGet.attributes.href = `cydia://url/https://cydia.saurik.com/api/share#?https://zerui18.github.io/ZX02/&package=${id}`;
+      modalElements.tweakGet.classList.remove("red");
+      modalElements.tweakGet.textContent = "Get";
+      break;
+    }
+    else if (i == details.packages.length-1) {
+      // Reached last element but no match, incompatible.
+      modalElements.tweakGet.attributes.href = '';
+      modalElements.tweakGet.classList.add("red");
+      modalElements.tweakGet.textContent = "Incompatible";
+    }
+  }
+
+  // Clear miscs.
+  const miscsElement = modalElements.tweakMiscs;
+  removeChildren(miscsElement);
+  // Create new miscs.
+  details.miscs.forEach(text => {
+    const misc = document.createElement("div");
+    misc.classList.add("tweak_misc");
+    misc.textContent = text;
+    miscsElement.appendChild(misc);
+  });
+
+  // Clear screens.
+  const screensElement = modalElements.tweakScreens;
+  removeChildren(screensElement);
+  // Create new screens.
+  // Beginning with demo video.
+  if (details.embedVideoURL) {
+    const iframe = document.createElement("iframe");
+    iframe.src = details.embedVideoURL;
+    iframe.classList.add("tweak_screen");
+    screensElement.appendChild(iframe);
+  }
+
+  // Then add the actual screenshots.
+  for (let i = 0; i < details.nScreenshots; i++) {
+    const img = document.createElement("img");
+    img.src = `./assets/screenshots/${tweak.id}-${i}.png`;
+    img.classList.add("tweak_screen");
+    screensElement.appendChild(img);
+  }
+
+  // Clear descriptions
+  const descriptionsElement = modalElements.tweakDescriptions;
+  removeChildren(descriptionsElement);
+  // Add descriptions
+  details.descriptions.forEach(description => {
+    // Add break
+    const breakElement = document.createElement("div");
+    breakElement.classList.add("break");
+    descriptionsElement.appendChild(breakElement);
+
+    // Add heading
+    const headingElement = document.createElement("div");
+    headingElement.classList.add("tweak_heading");
+    headingElement.textContent = description.title;
+    descriptionsElement.appendChild(headingElement)
+
+    // Add description
+    const descriptionElement = document.createElement("div");
+    descriptionElement.classList.add("tweak_desc");
+    descriptionElement.innerHTML = description.html;
+    descriptionsElement.appendChild(descriptionElement);
+  });
+}
+
+
+// Add a bubble in the main page for the given tweak info.
 function addTweak(tweak) {
   const contents = 
   `
-  <img src="assets/icons/${tweak.iconName}" style="width: 15vw; margin-right: 16px;" />
+  <img src="assets/icons/${tweak.id}.png" style="width: 15vw; margin-right: 16px;" />
   <div>
     <div class="tweak_name">${tweak.name}</div>
     <div class="tweak_desc">${tweak.description}</div>
@@ -33,17 +150,17 @@ function addTweak(tweak) {
     // Update overlay to display the selected tweak.
     setOverlayDisplaying(tweak);
     // Show overlay.
-    tweakOverlay.classList.remove("hidden");
-    tweakOverlayCard.classList.remove("hidden_below");
+    mainElements.tweakOverlayCard.classList.remove("hidden");
+    mainElements.main.classList.add("modal_shown");
   };
-  tweaksDiv.appendChild(newTweakDiv);
+  mainElements.tweaksDiv.appendChild(newTweakDiv);
 }
 
 // Add tweaks to display.
-tweaks.forEach(addTweak);
+TWEAKS.forEach(addTweak);
 
 // Configure overlay.
-tweakOverlay.onclick = () => {
-  tweakOverlayCard.classList.add("hidden_below");
-  tweakOverlay.classList.add("hidden");
+mainElements.tweakOverlayCard.onclick = () => {
+  mainElements.tweakOverlayCard.classList.add("hidden");
+  mainElements.main.classList.remove("modal_shown");
 };
---
+++ assets/tweaks.js
@@ -0,0 +1,47 @@
+const TWEAKS = [
+  {
+    name : "Frame",
+    description : "Bring your iDevice to life with your favourite video wallpapers!",
+    version : "2.0.0",
+    id : "frame",
+    details : {
+      subtitle : "Feature-complete Video Wallpaper",
+      packages : [
+        {
+          minOSVersion : [12, 2, 0],
+          id : "com.zx02.frame"
+        },
+        {
+          minOSVersion : [11, 0, 0],
+          id : "com.zx02.frame.nousrlibswift"
+        }
+      ],
+      miscs : [
+        "V2.0.0", "iOS 11-13"
+      ],
+      embedVideoURL : "https://streamable.com/s/djfvt/nustqr",
+      nScreenshots : 3,
+      descriptions : [
+        {
+          title : "Description",
+          html : "Ever wanted to see your favourite character/landscape live on your wallpaper? <br>\
+          Frame introduces video wallpapers with a comprehensive feature set to iOS. It's reliable and simple to use - with a few taps, you'll be all set.<br>"
+        },
+        {
+          title : "Setup",
+          html : "1. Install <br>\
+          2. Settings > Frame > Choose Wallpapers <br>\
+          3. Enjoy! <br>"
+        },
+        {
+          title : "Features",
+          html : "1. Setting different video wallpapers for the lock screen and home screen, each with a separate mute control. <br>\
+          2. Built-in video wallpaper catalogue that offers hundreds of HQ vertical video wallpapers at your fingertip. <br>\
+          3. Intelligent playback management that only plays the video when you're seeing it, thus conserving battery. <br>\
+          4. Alternative CLI <div class=\"code keyword\">framecli</div> for those who would like to use Frame with automated scripts. <br>\
+          5. Automatic Low Power Mode integration that deactivates Frame when LPM is on and reactivates it when LPM is off. <br>"
+        }
+      ]
+    }
+  }
+]
