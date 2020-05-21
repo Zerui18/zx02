ddf20fde5b1d20a1c90275f953314195ed842a41
Zerui Chen <chenzerui2002@gmail.com>
2020-05-21 16:53:34 +0800

    frame 2.2.0

--- assets/help.js
+++ assets/help.js
@@ -12,6 +12,17 @@ const HELP = {
         "Also, ensure that all parties are given read, write, execute permissions (accessible via the 'i' button for Filza).",
         "To verify that this problem has been resolved, go into Settings: exit the Frame tab if you've opened Frame's settings. Enter Frame's settings and verify that the error is not shown."
       ]
+    },
+    sysConfig: {
+      title : "Frame",
+      subtitle : "System Config Mismatch",
+      content : [
+        "This warning is shown whenever Frame detects that glitches would occur due to your current system wallpaper configurations.",
+        "To fix this, please set different system wallpapers for your lock screen and home screen.",
+        "<b>Tips</b>",
+        "If this warning appears despite you having the same video wallpaper set for the lock screen and home screen, it's likely that you didn't set them together using the setBoth option.",
+        "It would then be recommended that you reset both your video wallpapers using the setBoth option, or performance penalty would result as Frame would treat them as 2 different video wallpapers."
+      ]
     }
   }
 }
