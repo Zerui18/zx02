4cf411413e95428dcbda26568cd1aa377de55b28
Zerui18 <161558w@student.hci.edu.sg>
2023-05-09 20:34:37 +0800

    added: frame (3.0.1) & rootless

--- assets/tweaks.js
+++ assets/tweaks.js
@@ -28,18 +28,69 @@ const TWEAKS = [
   {
     name : "Frame",
     description : "Bring your iDevice to life with your favourite video wallpapers!",
-    version : "3.0.0",
+    version : "3.0.1",
     id : "frame",
     details : {
       subtitle : "Feature-complete Video Wallpaper",
       packages : [
         {
+          minOSVersion : [13, 8, 0],
+          id : "com.zx02.frame.ios14"
+        },
+        {
           minOSVersion : [13, 0, 0],
-          id : "com.zx02.frame"
+          id : "com.zx02.frame.ios13"
         }
       ],
       miscs : [
-        "v3.0.0", "iOS 13-14"
+        "v3.0.1", "iOS 13-14"
+      ],
+      embedVideoURL : "",
+      nScreenshots : 6,
+      descriptions : [
+        {
+          title : "Description",
+          html : "Ever wanted to see your favourite character/landscape live on your wallpaper? <br><br>\
+          Frame introduces video wallpapers with a comprehensive feature set to iOS. It's reliable and simple to use - with a few taps, you'll be all set.<br>"
+        },
+        {
+          title : "Setup",
+          html : "1. Install the package <br><br>\
+          2. Open the Frame app on Homescreen > Download Video > Choose Video <br><br>\
+          3. Enjoy! <br>"
+        },
+        {
+          title : "Features",
+          html : "1. Setting <b>different</b> video wallpapers for the lock screen and home screen, each with a separate mute control. <br><br>\
+          2. Built-in video wallpaper <b>catalogue</b> that offers hundreds of HQ vertical video wallpapers at your fingertip. <br><br>\
+          3. Intelligent <b>playback management</b> that only plays the video when you're seeing it, thus conserving battery. <br><br>\
+          4. Automatically <b>fade</b> your icons and status bar after a configurable period of inactivity. <br><br>\
+          5. Alternative CLI <b>framecli</b> for those who would like to use Frame with automated scripts. <br><br>\
+          6. Automatic <b>Low Power Mode</b> integration that deactivates Frame when LPM is on and reactivates it when LPM is off."
+        },
+        {
+          title : "Tips",
+          html : "1. If you would like to set the same video for both home and lock screen, try setting the same system wallpaper to both screens. It will give the effect where the wallpaper does not get lifted with the lock screen. <br><br>\
+          2. The downloaded video(s) & current video(s) can be found under <b>/var/mobile/Documents/com.zx02/frame/videos/</b> ."
+        }
+      ]
+    }
+  },
+  {
+    name : "Frame (Rootless)",
+    description : "Bring your iDevice to life with your favourite video wallpapers!",
+    version : "3.0.1",
+    id : "frame",
+    details : {
+      subtitle : "Feature-complete Video Wallpaper",
+      packages : [
+        {
+          minOSVersion : [14, 0, 0],
+          id : "com.zx02.frame.rootless"
+        }
+      ],
+      miscs : [
+        "v3.0.1", "iOS 14+"
       ],
       embedVideoURL : "",
       nScreenshots : 6,
